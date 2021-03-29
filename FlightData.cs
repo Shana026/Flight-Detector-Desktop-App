2162e5ac1aaad67cea28699e366f5d7f3b06646f
Yair <yairsh39@gmail.com>
2021-03-29 20:17:19 +0300

    basic structure

---
+++ FlightData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightDetector
+{
+    class FlightData
+    {
+        private Dictionary<int, float[]> _data;
+
+        private string[] _features;
+
+        public string[] Features => this._features;
+
+        FlightData(string xmlPath, string csvPath)
+        {
+            // todo implement
+            _data = new Dictionary<float, float[]>();
+            _features = new string[10]; // todo size of array
+        }
+
+        public float[] GetTimeStepData(int timeStep)
+        {
+            return this._data[timeStep];
+        }
+
+        public float GetFeatureValue(int timeStep, string feature)
+        {
+            // todo implement
+            return 1;
+        }
+    }
+}
