fbfb29692e31f8667cad624cd07083444b8ddcf4
Yair <yairsh39@gmail.com>
2021-03-31 10:52:25 +0300

    added Variance method

--- StatisticsUtil.cs
+++ StatisticsUtil.cs
@@ -8,38 +8,42 @@ namespace FlightDetector
 {
     static class StatisticsUtil
     {
-        public static double Expectancy(float[] xArray)
+        public static double Expectancy(double[] xArray)
         {
             // Todo Implement
             return 0;
         }
 
-        public static float Variance(float[] xArray)
+        public static double Variance(double[] xArray)
         {
             // Todo Implement
-            float expectancy = xArray.Average();
-            return 0;
+            double expectancy = xArray.Average();
+            double[] squared = SquareAllElements(xArray);
+            double squaredExpectancy = squared.Average();
+            return squaredExpectancy - Math.Pow(expectancy, 2);
         }
 
-        public static float Covariance(float[] xArray, float[] yArray)
-        {
-            // Todo Implement
-            return 0;
-        }
-
-        public static float Pearson(float[] xArray, float[] yArray)
+        public static double Covariance(double[] xArray, double[] yArray)
         {
             // Todo Implement
             return 0;
         }
 
-        private static float[] SquareAllElements(float[] xArray)
+        public static double Pearson(double[] xArray, double[] yArray)
         {
-            float[] res = new float[xArray.Length];
+            // Todo Implement
+            return 0;
+        }
+
+        private static double[] SquareAllElements(double[] xArray)
+        {
+            double[] res = new double[xArray.Length];
             for (int i = 0; i < xArray.Length; i++)
             {
                 res[i] = Math.Pow(xArray[i], 2);
             }
+
+            return res;
         }
     }
 }
